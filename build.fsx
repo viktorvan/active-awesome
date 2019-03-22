882ec6bbb822ba5e0eb528b48a65c61f224a4b35
Viktor Andersson <v.andersson@me.com>
2019-03-22 07:00:31 +0100

    Update script

--- build.fsx
+++ build.fsx
@@ -1,5 +1,6 @@
 #r "paket:
 nuget FSharp.Core 4.5.4
+nuget FSharp.Data
 nuget Fake.Api.Slack
 nuget Fake.Tools.Git
 nuget Fake.Core.Target //"
@@ -7,32 +8,42 @@ nuget Fake.Core.Target //"
 
 
 open Fake.Core
+open Fake.IO
 open Fake.Api
 open Fake.Tools
+open Fake.Core.TargetOperators
+open FSharp.Data
+
 
 let webhookUrl = "https://hooks.slack.com/services/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"
 let repositoryUrl = "https://github.com/viktorvan/active-awesome"
+let repositoryDir = "active-awesome"
 
 Target.create "Checkout" (fun _ ->
-    Git.Repository.clone "." repositoryUrl "active-awesome" 
+    Shell.cleanDir repositoryDir
+    Git.Repository.clone "." repositoryUrl repositoryDir
 )
 
 Target.create "Slack" (fun _ ->
-    let command = "git log -1 --pretty=%B"
-    let lastCommitMsg = 
-        match Git.CommandHelper.runGitCommand repositoryUrl command with
-        | true, stringList, str -> 
-            sprintf "%A : %s" stringList str
-        | false, _, _ -> ""
+    let getMessage = "log -1 --pretty=%B"
+    let getAuthor = "--no-pager show -s --format='%an' HEAD~1"
+    let lastCommitMsg = Git.CommandHelper.runSimpleGitCommand repositoryDir getMessage
+    let lastCommitAuthor = Git.CommandHelper.runSimpleGitCommand repositoryDir getAuthor
 
-    lastCommitMsg |> printf "%s"
+    if lastCommitMsg.StartsWith("tip:") then
+        let payload = lastCommitMsg.[4..]
 
-    // Slack.sendNotification webhookUrl (fun p ->
-    //     { p with
-    //         Text = "New tip!\n<https://github.com/viktorvan/active-awesome>!"
-    //         Channel = "#active-awesome-test"
-    //         IconEmoji = ":exclamation:"
-    //     }) |> printfn "Result: %s"
+        let text = sprintf "New tip from %s!\n%s\n%s" lastCommitAuthor payload repositoryUrl
+        Slack.sendNotification webhookUrl (fun p ->
+            { p with
+                Text = text
+                Channel = "#active-awesome-test"
+                IconEmoji = ":exclamation:"
+            }) |> printfn "Result: %s"
+    else ()
 )
+
+"Checkout" 
+    ==> "Slack"
 
 Target.runOrDefault "Slack"
